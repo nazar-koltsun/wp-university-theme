4fbec13d7e999755a2200ff9240e9c09aaa224aa
Nazar Koltsun <nazar-koltsun@ukr.net>
2023-09-18 17:59:34 +0300

    Added real dates to archive-event page

--- wp-content/themes/fictional-university-theme/archive-event.php
+++ wp-content/themes/fictional-university-theme/archive-event.php
@@ -14,16 +14,20 @@ get_header();
     </div>
   </div>
 </div>
-
 <div class="container container--narrow page-section">
   <?php
+  
   while (have_posts()) {
     the_post();
   ?>
     <div class="event-summary">
       <a class="event-summary__date t-center" href="<?php the_permalink() ?>">
-        <span class="event-summary__month"><?php the_time('M'); ?></span>
-        <span class="event-summary__day"><?php the_time('j'); ?></span>
+        <span class="event-summary__month">
+          <?php
+          $data = new DateTime(get_field('event_date'));
+          echo $data->format('M'); ?>
+        </span>
+        <span class="event-summary__day"><?php echo $data->format('d'); ?></span>
       </a>
       <div class="event-summary__content">
         <h5 class="event-summary__title headline headline--tiny">
@@ -35,13 +39,13 @@ get_header();
         </p>
       </div>
     </div>
-    <?php }
+  <?php }
   ?>
   <div class="pagination">
     <?php echo paginate_links(array(
-      'prev_text' => __( 'Previous', 'textdomain' ),
-      'next_text' => __( 'Next', 'textdomain' ),
-    ));?>
+      'prev_text' => __('Previous', 'textdomain'),
+      'next_text' => __('Next', 'textdomain'),
+    )); ?>
   </div>
 </div>
 
