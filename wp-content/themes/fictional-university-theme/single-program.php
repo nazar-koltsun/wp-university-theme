f9b2ea5867f7f7eade2e21d460f676618e9fa76c
Nazar Koltsun <nazar-koltsun@ukr.net>
2023-09-22 18:02:23 +0300

    Added related events to program page

--- wp-content/themes/fictional-university-theme/single-program.php
+++ wp-content/themes/fictional-university-theme/single-program.php
@@ -6,7 +6,7 @@
   <div class="page-banner__content container container--narrow">
     <h1 class="page-banner__title"><?php the_title(); ?></h1>
     <div class="page-banner__intro">
-      <p><?php echo get_post_meta(get_the_ID(), 'event_date', true ); ?></p>
+      <p><?php echo get_post_meta(get_the_ID(), 'event_date', true); ?></p>
     </div>
   </div>
 </div>
@@ -24,6 +24,54 @@
     </p>
   </div>
   <div class="generic-content"><?php the_content(); ?></div>
+
+  <?php
+      $today = date('Y-m-d');
+      $recent_events = new WP_Query(array(
+        'post_type' => 'event',
+        'posts_per_page' => 2,
+        'meta_key' => 'event_date',
+        'meta_query' => array(
+          array(
+            'key' => 'event_date',
+            'compare' => '>=',
+            'value' => $today,
+            'type' => 'DATE',
+          ),
+          array(
+            'key' => 'related_program',
+            'compare' => 'LIKE',
+            'value' => '"' . get_the_ID() . '"',
+          ),
+        ),
+        'orderby' => 'meta_value',
+        'order' => 'ASC',
+      ));
+
+      while ($recent_events->have_posts()) {
+        $recent_events->the_post();
+      ?>
+        <div class="event-summary">
+          <a class="event-summary__date t-center" href="<?php the_permalink() ?>">
+            <span class="event-summary__month">
+              <?php
+              $data = new DateTime(get_field('event_date'));
+              echo $data->format('M'); ?>
+            </span>
+            <span class="event-summary__day"><?php echo $data->format('d'); ?></span>
+          </a>
+          <div class="event-summary__content">
+            <h5 class="event-summary__title headline headline--tiny">
+              <a href="<?php the_permalink() ?>"><?php the_title() ?></a>
+            </h5>
+            <?php echo has_excerpt() ? get_the_excerpt() . '..' : wp_trim_words(get_the_content(), 10) ?>
+            <p>
+              <a href="<?php the_permalink() ?>" class="nu gray">Learn more</a>
+            </p>
+          </div>
+        </div>
+      <?php }
+      wp_reset_postdata() ?>
 </div>
 
 
