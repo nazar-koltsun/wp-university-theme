8d7d5f7915db2fbc736382c21f4afe9d5debbdb0
Nazar Koltsun <nazar-koltsun@ukr.net>
2023-09-22 18:01:22 +0300

    Added Events link

--- wp-content/themes/fictional-university-theme/header.php
+++ wp-content/themes/fictional-university-theme/header.php
@@ -27,19 +27,20 @@
                 About Us
               </a>
             </li>
-            <li><a href="#">Programs</a></li>
-            <li
+            <li 
               <?php
-                if (get_post_type() === 'event' or is_page('past-events')) echo "class=current-menu-item" ?>
-            >
+                if (get_post_type() === 'program' or is_page('programs')) echo "class=current-menu-item" ?>>
+              <a href="<?php echo get_post_type_archive_link('program') ?>">Programs</a>
+            </li>
+            <li <?php
+                if (get_post_type() === 'event' or is_page('past-events')) echo "class=current-menu-item" ?>>
               <a href="<?php echo get_post_type_archive_link('event') ?>">Events</a>
             </li>
             <li><a href="#">Campuses</a></li>
             <li <?php
                 if (
                   get_post_type() === 'post'
-                  ) echo "class=current-menu-item" ?>
-              >
+                ) echo "class=current-menu-item" ?>>
               <a href="<?php echo site_url('/blog') ?>">Blog</a>
             </li>
           </ul>
