1806896d40358218b27d783f96cc57411ccd1098
Nazar Koltsun <nazar-koltsun@ukr.net>
2023-09-22 12:28:26 +0300

    Added new Program post type

--- wp-content/mu-plugins/university-post-types.php
+++ wp-content/mu-plugins/university-post-types.php
@@ -1,6 +1,7 @@
 <?php
 
 function university_post_types() {
+  // Event Post Type
   $eventsLabels = array(
     'name'               => 'Events',
     'singular_name'      => 'Events',
@@ -29,6 +30,37 @@ function university_post_types() {
     // 'hierarchical'        => false,
     'menu_position'       => 5, // Position in the admin menu
     'supports'            => array('title', 'editor', 'excerpt'),
+  ));
+
+  // Program Post Type
+  $programsLabels = array(
+    'name'               => 'Programs',
+    'singular_name'      => 'Programs',
+    'menu_name'          => 'Programs',
+    'add_new'            => 'Add Program',
+    'add_new_item'       => 'Add New Program',
+    'edit_item'          => 'Edit Program',
+    'new_item'           => 'New Program',
+    'view_item'          => 'View Program',
+    'search_items'       => 'Search Programs',
+    'not_found'          => 'No Programs found',
+    'all_items'          => 'All Programs',
+    'not_found_in_trash' => 'No Programs found in trash',
+  );
+
+  register_post_type('program', array(
+    'labels'              =>  $programsLabels,
+    'menu_icon'           => 'dashicons-desktop',
+    'show_in_rest'        => true,
+    'public'              => true,
+    'has_archive'         => true,
+    'publicly_queryable'  => true,
+    'query_var'           => true,
+    'rewrite'             => array('slug' => 'programs'),
+    // 'capability_type'     => 'post',
+    // 'hierarchical'        => false,
+    'menu_position'       => 6, // Position in the admin menu
+    'supports'            => array('title', 'editor'),
   ));
 }
 
--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -31,6 +31,12 @@ function university_adjust_queries($query) {
     $query->set('orderby', 'meta_value');
     $query->set('order', 'ASC');
   }
+
+  if(!is_admin() and is_post_type_archive('program') and $query->is_main_query()) {
+    $query->set('orderby', 'title');
+    $query->set('order', 'ASC');
+    $query->set('posts_per_page', -1);
+  }
 }
 
 add_action('pre_get_posts', 'university_adjust_queries');
