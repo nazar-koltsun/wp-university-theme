f7ddd63e48376cdc9522cc2ed818023d00a0bfbe
Nazar Koltsun <nazar-koltsun@ukr.net>
2023-09-27 16:05:13 +0300

    'Profesor page' added featured image

--- wp-content/themes/fictional-university-theme/single-profesor.php
+++ wp-content/themes/fictional-university-theme/single-profesor.php
@@ -1,7 +1,9 @@
 <?php get_header(); ?>
 
 <div class="page-banner">
-  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>)">
+  <div class="page-banner__bg-image" style="
+    background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);
+  ">
   </div>
   <div class="page-banner__content container container--narrow">
     <h1 class="page-banner__title"><?php the_title(); ?></h1>
@@ -13,7 +15,14 @@
 
 <div class="container container--narrow page-section">
   <div class="generic-content">
-    <?php the_content(); ?>
+    <div class="row group">
+      <div class="one-third">
+        <?php the_post_thumbnail() ?>
+      </div>
+      <div class="two-thirds">
+        <?php the_content(); ?>
+      </div>
+    </div>
   </div>
   <?php
   $relatedPrograms = get_field('related_program');
