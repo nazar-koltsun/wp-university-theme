78cd3233a10144cefc78faba8a4c0e56e67a17f7
Nazar Koltsun <nazar-koltsun@ukr.net>
2023-10-02 17:57:35 +0300

    Dynamic background image to profesor page

--- wp-content/themes/fictional-university-theme/single-profesor.php
+++ wp-content/themes/fictional-university-theme/single-profesor.php
@@ -1,14 +1,18 @@
-<?php get_header(); ?>
+<?php get_header();
+  $backgroundImg = get_field('page_banner_image')['sizes']['pageBanner'];
+
+  $bannerBackgroundImage = $backgroundImg ? $backgroundImg : get_theme_file_uri('/images/ocean.jpg');
+?>
 
 <div class="page-banner">
   <div class="page-banner__bg-image" style="
-    background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);
+    background-image: url(<?php echo $bannerBackgroundImage ?>);
   ">
   </div>
   <div class="page-banner__content container container--narrow">
     <h1 class="page-banner__title"><?php the_title(); ?></h1>
     <div class="page-banner__intro">
-      <p><?php echo get_post_meta(get_the_ID(), 'event_date', true); ?></p>
+      <p><?php echo the_field('page_banner_subtitle') ?></p>
     </div>
   </div>
 </div>
