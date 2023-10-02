fd87a5668627fe78a645045e66a8e75bccef8583
Nazar Koltsun <nazar-koltsun@ukr.net>
2023-10-02 16:07:03 +0300

    Program page fixes

--- wp-content/themes/fictional-university-theme/single-program.php
+++ wp-content/themes/fictional-university-theme/single-program.php
@@ -44,11 +44,16 @@
     echo '<hr class="section-break">';
     echo '<h2 class="headline headline--medium">' . get_the_title() . ' Profesors:</h2>';
 
-    echo '<ul class="profesor-cards">';
+    echo '<ul class="professor-cards">';
     while ($program_profesors->have_posts()) {
       $program_profesors->the_post();
     ?>
-      <li><a href="<?php the_permalink() ?>"><?php the_title() ?></a></li>
+      <li class="professor-card__list-item">
+        <a class="professor-card" href="<?php the_permalink() ?>">
+          <img class="professor-card__image" src="<?php the_post_thumbnail_url('profesorLandscape') ?>" alt="">
+          <span class="professor-card__name"><?php the_title() ?></span>
+        </a>
+      </li>
     <?php } wp_reset_postdata() ?>
     
   <?php echo '</ul>'; } ?>
