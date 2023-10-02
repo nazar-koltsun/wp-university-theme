dba4290b8083a0dc77f73305b272cc64f3072958
Nazar Koltsun <nazar-koltsun@ukr.net>
2023-10-02 16:06:02 +0300

    Added new images fixes

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -14,6 +14,8 @@ function university_features()
 {
   add_theme_support('title-tag');
   add_theme_support('post-thumbnails');
+  add_image_size('profesorLandscape', 400, 260, true);
+  add_image_size('profesorPortrait', 480, 650, true);
 }
 
 add_action('after_setup_theme', 'university_features');
